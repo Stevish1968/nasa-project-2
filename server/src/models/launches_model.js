5aea3d30f8d3bb55f50d3fed603559a433e0609c
Stephen Little <47438829+Stevish1968@users.noreply.github.com>
2024-01-21 16:24:54 -0400

    Large update to code, have not pushed it up in a while.

---
+++ server/src/models/launches_model.js
@@ -0,0 +1,57 @@
+const {
+    httpGetAllLaunches,
+} = require('../routes/launches/launches.controller');
+
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Keplerr Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    target: 'Kepler-442 b',
+    customers: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    console.log(launches.get(launchId));
+    return launches.has(launchId);
+}
+
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success: true,
+            upcoming: true,
+            customers: ['ZTM', 'NASA'],
+            flightNumber: latestFlightNumber,
+        })
+    );
+}
+
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
+module.exports = {
+    launches,
+    getAllLaunches,
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
+};
